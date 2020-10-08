a94321cbb80544770cadbad6da20d8cd26d2a8cd
Maria Luiza de Souza MArinho <marinhomarialuiza0@gmail.com>
2020-10-08 18:10:50 -0300

    Integrando o ping e o schedule no bot

--- bot/bot.py
+++ bot/bot.py
@@ -1,4 +1,5 @@
 import discord
+import requests 
 
 client = discord.Client()
 
@@ -10,7 +11,28 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-    if message.content.startswith('Hello'):
-        await message.channel.send('Hello!')
+    if message.content.startswith('!Horários'):
+        mensagem = message.content
+        instrução = '!Horários'
+        args = mensagem[len(instrução) + 1:].split(' ')
 
-client.run("")
+
+        req_ping = requests.get('http://127.0.0.1:5000/ping')
+        req_schedule = requests.get('http://127.0.0.1:5000/schedule')
+        dic= req_schedule.json()
+
+        embed= discord.Embed(title="LCC Bot", descripition="Aulas do dia:")
+
+        for lcc in dic.keys():
+            if lcc.lower() in args or len(args):
+                if len(dic[lcc]) == 0:
+                    embed.add_field(name = lcc.upper(), value="Hoje não haverá aula", inline= False)
+                else:
+                    aula = dic[lcc][0]
+                    embed.add_field(name = lcc.upper(), value=("Hoje tem aula de %s ás %s horas.\n" %(aula['course'], aula['startTime'])), inline= False)
+
+        embed.set_footer(text="Guardians-DSC")
+
+        await message.channel.send(embed=embed)
+
+client.run("NzU0ODQ3ODI4Mjk5NDgxMDg4.X16s0g.yhHAKTSCJS-rWmO4APb2p9amXN4")
