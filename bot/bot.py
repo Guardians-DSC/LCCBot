c1351c38353721f173c319fdba4fcfa0f600928a
Maria Luiza de Souza MArinho <marinhomarialuiza0@gmail.com>
2020-10-09 18:17:35 -0300

    teste

--- bot/bot.py
+++ bot/bot.py
@@ -11,9 +11,11 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-    if message.content.startswith('!Horários'):
+    if message.content.startswith('Hello'):
+        await message.channel.send('Hello!')
+    if message.content.lower().startswith('!horarios'):
         mensagem = message.content
-        instrução = '!Horários'
+        instrução = '!horarios'
         args = mensagem[len(instrução) + 1:].split(' ')
 
 
@@ -27,12 +29,11 @@ async def on_message(message):
             if lcc.lower() in args or len(args):
                 if len(dic[lcc]) == 0:
                     embed.add_field(name = lcc.upper(), value="Hoje não haverá aula", inline= False)
-                else:
-                    aula = dic[lcc][0]
+                for aula in dic[lcc]:
                     embed.add_field(name = lcc.upper(), value=("Hoje tem aula de %s ás %s horas.\n" %(aula['course'], aula['startTime'])), inline= False)
 
         embed.set_footer(text="Guardians-DSC")
 
         await message.channel.send(embed=embed)
 
-client.run("")
+client.run("NzU0ODQ3ODI4Mjk5NDgxMDg4.X16s0g.5BBgr0DusyZ0CxLL4O_rsuvwoj4")
