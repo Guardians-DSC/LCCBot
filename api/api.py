4ded88e56b132b0765cadf5f2425d9e6f85fc747
Maria Luiza de Souza MArinho <marinhomarialuiza0@gmail.com>
2020-10-23 19:42:46 -0300

    alterações

--- api/api.py
+++ api/api.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask import jsonify
 from src.routes import ping_blueprint, scheduler_blueprint, status_blueprint
-
 app = Flask(__name__)
 app.register_blueprint(scheduler_blueprint)
 app.register_blueprint(ping_blueprint)
--- api/src/services/scheduleService.py
+++ api/src/services/scheduleService.py
@@ -1,8 +1,26 @@
+import paramiko
+import requests 
+from bs4 import BeautifulSoup
+from datetime import date
+from ..services.testes import *
+
+
 def scheduleService():
   LCC1 = []
-  LCC2 = [ 
-    { "course" : "Lab. de Prog2", "startTime": "10:00", "endTime": "12:00" },
-    { "course" : "Prog1", "startTime": "08:00", "endTime": "10:00" }
-  ]
+  LCC2 = [ ]
   LCC3 = []
   return {"LCC1": LCC1, "LCC2": LCC2, "LCC3": LCC3}
+
+site =  {"LCC1": "https://calendar.google.com/calendar/u/0/htmlembed?src=computacao.ufcg.edu.br_9u9fch5o55dbgrschdeduq348c@group.calendar.google.com&dates=" + SiteUrl(),
+"LCC2": "https://calendar.google.com/calendar/u/0/htmlembed?src=computacao.ufcg.edu.br_80qc5chl59nmv2268aef8hp528@group.calendar.google.com&dates=" + SiteUrl(),
+"LCC3": "https://calendar.google.com/calendar/u/0/htmlembed?src=computacao.ufcg.edu.br_noalttgqttm3c5pm94k3ttbj1k@group.calendar.google.com&dates=" + SiteUrl() }
+
+
+events = get_events(site)
+today = date.today()
+curr_day = today.strftime('%d/%m/%Y')
+new_event = SpecificEvent(events, curr_day)
+
+LCC1 = new_event
+LCC2 = new_event
+LCC3 = new_event
