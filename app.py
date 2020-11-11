67d0c78658d6c66cd23f371e772c0ed5cc27a9e1
Thúlio Carvalho <thulio.carvalho@ccc.ufcg.edu.br>
2020-11-11 20:04:59 -0300

    wip: changed api.py name and references in procfile

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask import jsonify
+from api.src.routes import ping_blueprint, scheduler_blueprint, status_blueprint
+app = Flask(__name__)
+app.register_blueprint(scheduler_blueprint)
+app.register_blueprint(ping_blueprint)
+app.register_blueprint(status_blueprint)
+
+if __name__ == "__main__":
+    app.run()
